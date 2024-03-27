432b4eab916fbd127b6ef4997b2e08940763f9df
ItamarHazan <itamarhazan456@gmail.com>
2024-03-27 16:33:00 +0200

    first commit

---
+++ Play.Catalog/src/Play.Catalog.Service/Program.cs
@@ -0,0 +1,30 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+using Play.Catalog.Service.Repositories;
+using Play.Catalog.Service.Settings;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMongo().AddMongoRepository<Item>("items");
+var app = builder.Build();
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseHttpsRedirection();
+
+app.Run();
